313989b56aa092eb87e59d2d8aacb78834d64eb2
MIST Analytics <46724952+MISTAnalytics@users.noreply.github.com>
2019-01-17 21:14:02 +0100

    Create webtrekk-conversions.js

---
+++ tags/webtrekk-conversions.js
@@ -0,0 +1,15 @@
+<script>
+
+//Fill fdf
+
+  	var el = {{Click Element}};
+    var selection = el.querySelector('#input_querySelector');
+  
+	webTrekk.sendinfo({
+      	linkId: 'Conversion click',
+      	customEcommerceParameter: { '1': selection,
+                                    '2': '{{GCLID}}',
+                                    '3': '{{YYYY-MM-DD hh-mm-ss}}'
+                                  }
+	});
+</script>
