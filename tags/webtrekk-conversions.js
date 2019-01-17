0f3d353e7ea52041321fea405bb8ed2995b4a389
MIST Analytics <46724952+MISTAnalytics@users.noreply.github.com>
2019-01-17 21:23:47 +0100

    Update webtrekk-conversions.js

--- tags/webtrekk-conversions.js
+++ tags/webtrekk-conversions.js
@@ -1,12 +1,14 @@
 <script>
 
-//Fill fdf
+//Fill the CSS query selector instead of '#input_querySelector'.
+//Change the IDs 1, 2 and 3 to match your own Webtrekk configuration.
+//Remove the comment signs '//' to include the measurement of a Webtrekk event. The default value is 'Conversion click'.
 
   var el = {{Click Element}};
-    var selection = el.querySelector('#input_querySelector');
+  var selection = el.querySelector('#input_querySelector');
   
 	webTrekk.sendinfo({
-      	linkId: 'Conversion click',
+      	//linkId: 'Conversion click',
       	customEcommerceParameter: { '1': selection,
                                     '2': '{{GCLID}}',
                                     '3': '{{YYYY-MM-DD hh-mm-ss}}'
